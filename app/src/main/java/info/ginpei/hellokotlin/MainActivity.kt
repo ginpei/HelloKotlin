5593eb5ae91d03065b26684d4cf075d68616123d
ginpei <ginpei@gmail.com>
2017-07-21 22:19:52 -0700

    print hello in log using kotlin android extensions

--- app/src/main/java/info/ginpei/hellokotlin/MainActivity.kt
+++ app/src/main/java/info/ginpei/hellokotlin/MainActivity.kt
@@ -2,11 +2,17 @@ package info.ginpei.hellokotlin
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import android.util.Log
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        helloButton.setOnClickListener {
+            Log.d("MainActivity", "Hello!")
+        }
     }
 }
